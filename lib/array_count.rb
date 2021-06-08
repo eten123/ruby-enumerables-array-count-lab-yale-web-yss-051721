04888ebeeefac7a2b344b317aa1a46e912f041f9
dashing-utility-e4d8 <eten.uket@yale.edu>
2021-06-08 03:52:36 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -2,11 +2,13 @@ def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
   length = array.length
   index = 0
+  total = 0
   while index < length do
-    index += 1
+    total += 1
 end
-index
+index += 1
 end
+
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
 end
